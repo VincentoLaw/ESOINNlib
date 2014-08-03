cf2c158fa9a7f02061ac3c600f465d198842cbcb
Panchos39 <shirkin39@rambler.ru>
2014-08-03 20:02:17 +0300

    Nothing specific changes

--- neuron.h
+++ neuron.h
@@ -6,15 +6,20 @@
 class Connection;
 
 class Neuron{
-private:
-    double density;
-    double similarityThreshold;
-    int dimentionSize;
-    int classId;
-    double * values;
-    std::list<Connection> * neighboursList;
-    int winnerTimesCount;
-public:
-    Neuron(int dimentionSize, double * values);
-    void updateDensity();
+	
+	private:
+    	
+		double density;
+    	double similarityThreshold;
+    	int dimentionSize;
+    	int classId;
+    	double * weights;
+    	std::list<Connection> * neighboursList;
+    	int winnerTimesCount = 0;
+	
+	public:
+    	
+		Neuron(int dimentionSize, double * values);
+    	void updateDensity();
+    	void incSignal(int n = 1);
 };
