551dc8baa2192f34ffc5e2236d9d8af544775afc
Panchos39 <shirkin39@rambler.ru>
2014-08-03 19:41:37 +0300

    Added description of 3 methods: increaseAge, setAge, getId
    And also added static field - count, which includes acount of neurons in
    a network

--- connection.cpp
+++ connection.cpp
@@ -3,4 +3,19 @@
 Connection::Connection(neuron * first, neuron * second){
     this.first = first;
     this.second = second;
+    this::count++;
+    this.id = this::count;
+    
+}
+
+void Connection::increaseAge(int n = 1){
+	this.age += n;
+}
+
+void Connection::setAge(int age){
+	this.age = age;
+}
+
+int Connection::getId(){
+	return this.age;
 }
