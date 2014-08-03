1bc9965282769f53593cfa744cc6a8dc09fddcfd
Panchos39 <shirkin39@rambler.ru>
2014-08-03 19:59:28 +0300

    Moved some parts of code for the convenience and added some methods and fields
    Added some methods and fields:
    fields: maximalConnectonAge;
    methods: keytoConnect, findSecondWiner, calcEuclidNorm.
    Reloaded methods : setConnectionAge, getConnection age, getConnection
    with other parameters.
    Methods, become  private:
    Neuron * getNeuron(int neuronIndex);
    int getNeuronIndex(Neuron * neuron);
    int getConnectionIndex(Connection * connection);
    Connection * getConnection(int connectionIndex);
    int getConnectionAge(int connectionIndex);
    int getConnectionAge(Connection * connection);

--- esoinn.h
+++ esoinn.h
@@ -2,8 +2,12 @@
 #include "neuron.h"
 #include "connection.h"
 #include <list>
+#include <math.h>
 #include <string>
+#include <vector>
+#include <stack>
 
+using namespace std;
 /*
 Neuron can be changed to Node
 Connection can be changed to Edge
@@ -15,54 +19,86 @@ variables names starting with small letter
 */
 
 class Esoinn{
-    private:
-        //variables
+    
+	private:
+        
+		//variables
         int dimensionSize;
-        std::list<Neuron> * neuronsList;
-        std::list<Connection> * connectionsList;
-        //store Neurons, that represent classes
-        std::list<Neuron> classesList;
-        //methods
+        int maximalConnectionAge;
+        
+        
+        list<Neuron> * neuronsList;
+        list<Connection> * connectionsList;
+        
+		//store Neurons, that represent classes
+        list<Neuron> classesList;
+        
+		//methods
         double commonDistanceFunction(double * inputVector, double * checkDistanceVector);
-        //TODO another params?
+        
+		//TODO another params?
         void addNeuron(Neuron * neuronToAdd);
-        //TODO another params?
+        
+		//TODO another params?
         void removeNeuron(Neuron * neuronToRemove);
         void addConnection(Neuron* first, Neuron * second);
         void removeConnection(Neuron * first, Neuron * second);
         bool connectionExist(Neuron * first, Neuron * second);
-        std::list<Neuron> getNeighbors(Neuron * neuron);
-        double calcMeanDistance(Neuron * neuron);
+        bool keytoConnect(Neuron * first, Neuron * second);
+        
+		list<Neuron> getNeighbors(Neuron * neuron);
+        
+		double calcMeanDistance(Neuron * neuron);
         double calcPoint(Neuron * neuron);
-        //TODO: params and implementation
+       	double calcEuclidNorm(int * vector); 
+		//TODO: params and implementation
         void separateToSubclasses();
-        void findWiner(double inputVector);
-        double distance(Neuron * first, Neuron * second);
+        Neuron* findWiner(double inputVector);
+        Neuron* findSecondWiner(double inputVector, Neuron * firstWiner);
+        
+		double distance(Neuron * first, Neuron * second);
+        
+        Neuron * getNeuron(int neuronIndex);
+        
+		int getNeuronIndex(Neuron * neuron);
+        
+		int getConnectionIndex(Connection * connection);
+        int getConnectionIndex(Neuron * first, Neuron * second);
+        
+		Connection * getConnection(Neuron * first, Neuron * second);
+        Connection * getConnection(int connectionIndex);
+        
+		int getConnectionAge(int connectionIndex);
+        int getConnectionAge(Connection * connection);
+        int getConnectionAge(Neuron * first, Neuron * second);
+        
+        int setConnectionAge(int connectionIndex, int age = 0);
+        int setConnectionAge(Connection * connection, int age = 0);
+        int setConnectionAge(Neuron * first, Neuron * second, int age = 0);
+        
+        
+
     public:
-        //Constructor for ESOINN
+        
+		//Constructor for ESOINN
         //dimensionSize means the size of learning vectors
         //distanceFunction can not be set, it means the function, that calculate distance between vectors
-        Esoinn(int dimensionSize, double learningRate, int maximalConnectionAge, int lambda, double (*distanceFunction)(double *,double *));
-        //method for input learning vectors as double values
+        
+		Esoinn(int dimensionSize, double learningRate, int maximalConnectionAge, int lambda, double (*distanceFunction)(double *,double *));
+        
+		//method for input learning vectors as double values
         void inputSignal(double * inputVector);
         void inputSignal(Neuron * inputVector);
-        //returns main neuron that represent this input vector
+        
+		//returns main neuron that represent this input vector
         Neuron * classify(double * inputVector);
-        int neuronClassId(double * inputVector);
+        
+		int neuronClassId(double * inputVector);
         int getNodesCount();
         int getConnectionsCount();
-
-        //I thought it should be private functions
-        //?
-        Neuron * getNeuron(int neuronIndex);
-        int getNeuronIndex(Neuron * neuron);
-        int getConnectionIndex(Connection * connection);
-        Connection * getConnection(int connectionIndex);
-        int getConnectionAge(int connectionIndex);
-        int getConnectionAge(Connection * connection);
-
-        bool saveNetworkData(std::string fileName);
-        bool loadNetworkData(std::string fileName);
+        
+		bool saveNetworkData(string fileName);
+        bool loadNetworkData(string fileName);
 
 };
 
