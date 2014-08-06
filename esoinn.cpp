786636ef7a37211c31af1c1fa8d68b6612b584d0
Panchos39 <shirkin39@rambler.ru>
2014-08-06 23:29:02 +0300

    Given descriptions of some methods
    Given description of:
    calcEuclidNorm;
    calcMeanDistance
    calcPoint
    8 part of algorithm is completed

--- esoinn.cpp
+++ esoinn.cpp
@@ -1,10 +1,35 @@
-#include "neuron.h"``
+#include "neuron.h"
 #include "esoinn.h"
 
 Esoinn::Esoinn(int dimensionSize, double learningRate, int maximalConnectionAge, int lambda, double (*distanceFunction)(double *,double *)){//= &commonDistanceFunction
     this->dimensionSize = dimensionSize;
     neuronsList = new std::list<Neuron>();
 }
+
+double Esoinn::calcEuclidNorm(double * vector1, double * vector2, int n){
+	double res = 0.0;
+	for(int i = 0; i < n; i++){
+		res += pow(vector1[i] - vector2[i], 2);
+	}
+	res = pow(res, 0.5);
+}
+
+double Esoinn::calcMeanDistance(Neuron * neuron){
+	double res = 0.0;
+	Neuron * tmp;
+	for(int i = 0; i < neuron->neighboursList->size(); i++){
+		tmp = neuron->neighboursList[i].getNeighbourNeuron();
+		res += calcEucludNorm(neuron->weights, tmp->weight, this->dimensionSize);
+	}
+	res /= neuron->neighboursList->size();
+	
+	return res;
+}
+
+double Esoinn::calcPoint(Neuron * neuron){
+	return 1 / pow(1 + calcMeanDistance(neuron), 2);
+}
+
 
 //TODO: implement this function
 void Esoinn::inputSignal(double * inputVector){
@@ -19,7 +44,7 @@ void Esoinn::inputSignal(double * inputVector){
 	for(int i = 0; i < a1->neighboursList.size(); i++){
 			a1->neighboursList[i]->increaseAge();
 	}
-/*-----------------end-of-5.-------------------------------------------------------*/
+/*-----------------5-end.----------------------------------------------------------*/
 
 /*-----------------6.To-create-connections-between-a1-and-a2-if necessary----------*/
 	bool exist = connectionExist(a1, a2);
@@ -37,16 +62,25 @@ void Esoinn::inputSignal(double * inputVector){
 	else{
 		if(exist) removeConnection(a1, a2);
 	}
-/*-----------------end-of-6--------------------------------------------------------*/
+/*-----------------6.end.----------------------------------------------------------*/
 
 /*-----------------7.Increase-signals-of--neuron-winner----------------------------*/
 	a1->incSignal();
-/*-----------------end-of-7--------------------------------------------------------*/
+/*-----------------7-end.----------------------------------------------------------*/
 
 /*-----------------8.Update the density of winner----------------------------------*/
 	double point = calcPoint(a1);
-	
-/*-----------------end-of-8--------------------------------------------------------*/
+	double density = point / a1->winerTimesCount;
+	a1->setDensity(density);
+/*-----------------8-end.----------------------------------------------------------*/
+
+/*-----------------9.Find-old-edges-and-remove-them--------------------------------*/
+	for(int i = 0; i < connectionsList.size(); i++){
+		if (connectionsList[i]->age > this.maximalConnectionAge){
+			connectionsList->remove(connectionList[i]);
+		}
+	}
+/*-----------------9-end.----------------------------------------------------------*/
 }
 
 
