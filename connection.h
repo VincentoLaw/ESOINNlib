81bdf1c99369bd20c39f89ee7c3f57e59c4b0b4f
Panchos39 <shirkin39@rambler.ru>
2014-08-06 23:24:33 +0300

    Added getNeighbourNeuron

--- connection.h
+++ connection.h
@@ -1,20 +1,24 @@
-#pragma once
+//#pragma once
 #include "neuron.h"
 
-class Neuron;
 
 //Also can be called as edge
 class Connection{
-    private:
-        int age;
+    
+	private:
+        
+		int age;
         int id;
-        static int count = 0;
-    public:
-        Neuron * first, * second;
+        static int count;
+    
+	public:
+        
+		Neuron * first, * second;
 
         Connection(Neuron * first, Neuron * second);
         //increasing age of connection by n
         void increaseAge(int n = 1);
+        Neuron* getNeighbourNeuron();
         void setAge();
         int getId();
 };
