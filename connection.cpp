3e0edc70b9bef26e72e96725b5f1c5ba7461e674
Panchos39 <shirkin39@rambler.ru>
2014-08-13 15:57:03 +0300

    Some corrections

--- connection.cpp
+++ connection.cpp
@@ -1,23 +1,23 @@
 #include "connection.h"
 
 Connection::Connection(neuron * first, neuron * second){
-    this.first = first;
-    this.second = second;
+    this->first = first;
+    this->second = second;
     this::count++;
-    this.id = this::count;
+    this->id = this::count;
     
 }
 
 void Connection::increaseAge(int n = 1){
-	this.age += n;
+	this->age += n;
 }
 
 void Connection::setAge(int age){
-	this.age = age;
+	this->age = age;
 }
 
 int Connection::getId(){
-	return this.age;
+	return this->age;
 }
 
  Neuron* Connection::getNeighbourNeuron(){
