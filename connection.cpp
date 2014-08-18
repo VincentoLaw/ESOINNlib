1134ede234f2b722faf1cca288cea4b67a839a6b
Panchos39 <shirkin39@rambler.ru>
2014-08-18 21:37:00 +0300

    Some syntax correction

--- connection.cpp
+++ connection.cpp
@@ -1,13 +1,12 @@
 #include "connection.h"
+#include "neuron.h"
 
-Connection::Connection(neuron * first, neuron * second){
+Connection::Connection(Neuron * first, Neuron * second){
     this->first = first;
     this->second = second;
-    this::count++;
-    this->id = this::count;
-    
-}
-
+    count++;
+    this->id = count;
+}		  
 void Connection::increaseAge(int n = 1){
 	this->age += n;
 }
@@ -17,9 +16,13 @@ void Connection::setAge(int age){
 }
 
 int Connection::getId(){
+	return this->id;
+}
+
+int Connection::getAge(){
 	return this->age;
 }
 
- Neuron* Connection::getNeighbourNeuron(){
+Neuron* Connection::getNeighbourNeuron(){
 	return this->first == this ? this->second : this->first; 
 }
