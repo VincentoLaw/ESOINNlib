06c5d206cb1b8ee378e26dc45ba576bd22a3d55f
Panchos39 <shirkin39@rambler.ru>
2014-08-31 16:16:31 +0300

    Given description of method remove
    Added some syntax corrections

--- cluster.cpp
+++ cluster.cpp
@@ -20,23 +20,28 @@ Neuron * Cluster::getApex(){
 
 void Cluster::calcMeanDensity(){
 	double res = 0.0;
-    for (std::list<Neuron>::iterator it=this->neuronsList->begin(); it != this->neuronsList->end(); ++it){
-        res += (*it).getDensity();
+    for (std::list<Neuron*>::iterator it=this->neuronsList->begin(); it != this->neuronsList->end(); ++it){
+        res += (*it)->getDensity();
     }
 	res /= this->neuronsList->size();
 	this->meanDensity = res;
 }
 void Cluster::findApex(){
-	for (std::list<Neuron>::iterator it=this->neuronsList->begin(); it != this->neuronsList->end(); ++it){
-		if ((*it).getDensity() > this->apex->getDensity()) this->apex = it;
+	for (std::list<Neuron*>::iterator it = this->neuronsList->begin(); it != this->neuronsList->end(); ++it){
+		if ((*it)->getDensity() > this->apex->getDensity()) this->apex = (*it);
 		//this->apex = (*it).getDensity() > this->apex->getDensity() ? it : this->apex;
 	}
 }
 
+void Cluster::remove(){
+	delete this;
+}
+
 void Cluster::unite(Cluster* A, Cluster* B){
-	std::list<Neuron>::iterator it = A->neuronsList->end();
-	A->neuronsList->splice(it, B->neuronsList);
+	std::list<Neuron*>::iterator it = A->neuronsList->end();
+	A->neuronsList->splice(it, (*B->neuronsList));
 	A->calcMeanDensity();
 	A->findApex();
 	B->remove();
 }
+
